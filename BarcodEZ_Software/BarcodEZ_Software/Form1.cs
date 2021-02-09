04c9a2d48bb29733e868aa65ef548d58de518430
Brozzoni <62284587+Brozzoni@users.noreply.github.com>
2021-02-09 12:57:27 +0100

    Inizio

--- BarcodEZ_Software/BarcodEZ_Software/Form1.cs
+++ BarcodEZ_Software/BarcodEZ_Software/Form1.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AForge.Video.DirectShow;
+using ZXing;
 
 namespace BarcodEZ_Software
 {
     public partial class Form1 : Form
-    {
+    { 
         public Form1()
         {
             InitializeComponent();
         }
+
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo device in filterInfoCollection)
+            {
+                cboCamera.Items.Add(device.Name);
+            }
+                
+            cboCamera.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cboCamera.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
     }
 }
