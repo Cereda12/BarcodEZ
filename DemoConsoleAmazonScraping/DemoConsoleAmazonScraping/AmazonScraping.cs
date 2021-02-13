3b5a35ed234e59139a1c417729b7801e9ad738c6
TommasoStroppa <TommasoStroppa@users.noreply.github.com>
2021-02-13 12:54:32 +0100

    Aggiunta classe AmazonScraping

---
+++ DemoConsoleAmazonScraping/DemoConsoleAmazonScraping/AmazonScraping.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace DemoConsoleAmazonScraping
+{
+    class AmazonScraping
+    {
+        public static string GetRequest(string url)
+        {
+            string strhtml = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                strhtml = reader.ReadToEnd();
+            }
+            return strhtml;
+        }
+        public static object DataParse(string strHtml)
+        {
+            string Details = string.Empty;
+            string Asin = string.Empty;
+            string Name = string.Empty;
+            string Price = string.Empty;
+            string Description = string.Empty;
+
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(strHtml);
+            htmlDocument.DocumentNode.Descendants()
+                .Where(n => n.Name == "script" || n.Name == "style")
+                .ToList()
+                .ForEach(n => n.Remove());
+
+            try
+            {
+                Details = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='detailBullets_feature_div']").InnerText.ToString();
+                Asin = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='detailBullets_feature_div']").InnerText.ToString().Substring(Details.IndexOf("ASIN\n:\n") + 8, 10);
+                Name = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='productTitle']").InnerText.Trim();
+                Price = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='a-color-price']").InnerText.Trim();
+                Description = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='productDescription']//p").InnerText.Trim();
+            }
+            catch
+            {
+                Name = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='productTitle']").InnerText.Trim();
+                Price = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='a-color-price']").InnerText.Trim();
+            }
+            Price.Remove(Price.IndexOf(','), 2);
+            
+
+            return new
+            {
+                asin = Asin,
+                name = Name,
+                price = Price,
+                description=Description
+            };
+        }
+    }
+}
---
+++ DemoConsoleAmazonScraping/DemoConsoleAmazonScraping/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace DemoConsoleAmazonScraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int scelta;
+            string url1 = "https://www.amazon.it/Eastpak-Astuccio-Unisex-Taglia-5x22x9/dp/B000VZK4FK/ref=mp_s_a_1_2?dchild=1&keywords=eastpak+astuccio&qid=1612001436&sr=8-2";
+            string url2 = "https://www.amazon.it/STABILO-ORIGINAL-Evidenziatore-colore-Giallo/dp/B0017YOQ44/ref=sr_1_10?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=stabilo+boss&qid=1613216368&sr=8-10";
+            string strHtml = string.Empty;
+            scelta = int.Parse(Console.ReadLine());
+            switch(scelta)
+            {
+                case 1:
+                    {
+                        Console.WriteLine($"Fetch Data From URL {url1}");
+                        strHtml = AmazonScraping.GetRequest(url1);
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.WriteLine($"Fetch Data From URL {url2}");
+                        strHtml = AmazonScraping.GetRequest(url2);
+                        break;
+                    }
+            }            
+            object result = AmazonScraping.DataParse(strHtml);
+            Console.WriteLine("Result :");
+            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+            Console.ReadLine();
+        }
+    }
+}
