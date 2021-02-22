40ca9792f97acf2911f5024e6290e5281754c709
TommasoStroppa <62284391+TommasoStroppa@users.noreply.github.com>
2021-02-22 09:42:54 +0100

    Aggiunta test WebResponse

--- DemoConsoleAmazonScraping/TestScraping/Test_ClasseAmazonScraping.cs
+++ DemoConsoleAmazonScraping/TestScraping/Test_ClasseAmazonScraping.cs
@@ -1,14 +1,22 @@
-using System;
+using System.Net;
 using Xunit;
+using FluentAssertions;
 
 namespace TestScraping
 {
     public class Test_ClasseAmazonScraping
     {
         [Fact]
-        public void Test1()
+        public void WebResponseTest()
         {
+            //Arrange
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("www.amazon.it");
 
+            //Act
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //Assert
+            response.Should().Be(request.GetResponse());
         }
     }
 }
