b0c93d72679655ead726dba21696e1ed95356425
TommasoStroppa <62284391+TommasoStroppa@users.noreply.github.com>
2021-02-22 09:17:20 +0100

    Aggiunta progetto testing e classe test di AmazonScraping

---
+++ DemoConsoleAmazonScraping/TestScraping/Test_ClasseAmazonScraping.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TestScraping
+{
+    public class Test_ClasseAmazonScraping
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
