ba30c456b91015d2268cc769e0b430d759707ab4
TommasoStroppa <62284391+TommasoStroppa@users.noreply.github.com>
2021-03-02 13:07:40 +0100

    Continuo sviluppo amazon scraping

--- DemoConsoleAmazonScraping/TestScraping/AmazonScraping.cs
+++ DemoConsoleAmazonScraping/TestScraping/AmazonScraping.cs
@@ -37,8 +37,6 @@ namespace DemoConsoleAmazonScraping
         /// <returns></returns>
         public static object DataParse(string strHtml)
         {
-            AmazonProduct product;
-
             string Name = string.Empty;
             string Price = string.Empty;
             string Description = string.Empty;
@@ -55,56 +53,12 @@ namespace DemoConsoleAmazonScraping
             Price = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='a-color-price']").InnerText.Trim();
             Description = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='productDescription']//p").InnerText.Trim();
 
-            product = new AmazonProduct(Name, Price, Description);
-
-            return product;
-        }
-    }
-    class AmazonProduct
-    {
-        private string _name;
-        public string name
-        {
-            get { return _name; }
-            set
+            return new
             {
-                if (string.IsNullOrEmpty(value))
-                    throw new Exception("Stringa non valida");
-                _name = value;
-            }
-        }
-        private string _price;
-        public string price
-        {
-            get { return _price; }
-            set
-            {
-                decimal result;
-                if (!decimal.TryParse(value, out result))
-                    throw new Exception("Stringa non valida");
-                if (result <= 0)
-                    throw new Exception("Prezzo non valido");
-                _price = result.ToString();
-            }
-        }
-        private string _description;
-        public string description
-        {
-            get { return _description; }
-            set
-            {
-                if (string.IsNullOrEmpty(value))
-                    throw new Exception("Stringa non valida");
-                _description = value;
-            }
-        }
-        public AmazonProduct(string Name, string Price, string Description)
-        {
-            if (string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Price) || string.IsNullOrEmpty(Description))
-                throw new Exception("Stringa non valida");
-            this.name = Name;
-            this.price = Price;
-            this.description = Description;
+                name = Name,
+                price = Price,
+                description=Description
+            };
         }
     }
 }
--- DemoConsoleAmazonScraping/TestScraping/Test_ClasseAmazonScraping.cs
+++ DemoConsoleAmazonScraping/TestScraping/Test_ClasseAmazonScraping.cs
@@ -1,6 +1,7 @@
 using System.Net;
 using Xunit;
 using FluentAssertions;
+using DemoConsoleAmazonScraping;
 
 namespace TestScraping
 {
@@ -17,6 +18,21 @@ namespace TestScraping
 
             //Assert
             response.Should().Be(request.GetResponse());
+        }
+        [Fact]
+        public void Layout1Test()
+        {
+            //Arrange
+            string url= "https://www.amazon.it/Eastpak-Astuccio-Unisex-Taglia-5x22x9/dp/B000VZK4FK/ref=mp_s_a_1_2?dchild=1&keywords=eastpak+astuccio&qid=1612001436&sr=8-2";
+            string html = string.Empty;
+            result = string.Empty;
+
+            //Act
+            html=AmazonScraping.GetRequest(url);
+            result = AmazonScraping.DataParse(html);
+
+            //Assert
+
         }
     }
 }
