30c6a26c95f689d748ad8d9505c0a4048457d256
Cereda12 <62284582+Cereda12@users.noreply.github.com>
2021-03-04 12:56:05 +0100

    Sistemazione Post-Merge

---
+++ BarcodEZ_Software/BarcodEZ_Software/ClasseAPI.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace BarcodEZ_Software
+{
+    class ClasseAPI
+    {
+
+        #region Chiave API
+        private static string key = "e3a9ddf2d3mshdc71524468db118p1d82edjsnb5391767148e";
+        #endregion
+
+        public static int ricerche = 0;
+
+        /// <summary>
+        /// Effettua una chiamata all'API richiedendo l'Asin del codice a barre dato come parametro
+        /// </summary>
+        /// <param name="ean">Numero del codice a barre</param>
+        /// <returns> Codice Asin dell'EAN corrispondente</returns>
+        public static string ReqAsin(string ean)
+        {
+            if (ricerche > 5)
+                throw new Exception("Troppe richieste");
+
+            var client = new RestClient($"https://amazon-price1.p.rapidapi.com/upcToAsin?upc={ean}&marketplace=ES");
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-key", key);
+            request.AddHeader("x-rapidapi-host", "amazon-price1.p.rapidapi.com");
+            IRestResponse response = client.Execute(request);
+
+            ricerche++;
+
+            var Risp = (Risposta)JsonConvert.DeserializeObject<Risposta>(response.Content);
+
+            if (string.IsNullOrEmpty(Risp.Asin))
+                return "Prodotto non trovato";
+
+            return Risp.Asin;
+        }
+        public class Risposta
+        {
+            public string Asin { get; set; }
+        }
+
+        /// <summary>
+        /// Compone il link per effettuare la ricerca su amazon 
+        /// </summary>
+        /// <param name="asin">Asin per effettuare la composizione del link</param>
+        /// <returns>Link di ricerca amazon del prodotto</returns>
+        public static string ReqLink(string asin)
+        {
+            return $"https://www.amazon.es/dp/{asin}";
+        }
+    }
+}
---
+++ BarcodEZ_Software/XUnitTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+using System;
+using Xunit;
+
+namespace XUnitTest
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Prova a pingare il sito di RapidApi
+        /// </summary>
+        [Fact]
+        public void Test1()
+        {
+            //Arrange
+            var ping = new System.Net.NetworkInformation.Ping();
+
+            //Act
+            var result = ping.Send("www.rapidapi.com");
+
+            //Assert
+            result.Status.Should().Be(System.Net.NetworkInformation.IPStatus.Success);
+        }
+    }
+}
