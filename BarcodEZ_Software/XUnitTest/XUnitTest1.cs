13fed047e9254e54b428257ae669f79cc95ade4c
Cereda12 <62284582+Cereda12@users.noreply.github.com>
2021-03-16 12:58:52 +0100

    Correzione

--- BarcodEZ_Software/XUnitTest/XUnitTest1.cs
+++ BarcodEZ_Software/XUnitTest/XUnitTest1.cs
@@ -1,18 +1,16 @@
 using System;
-using System.Net;
 using Xunit;
 using FluentAssertions;
 
 namespace XUnitTest
 {
-<<<<<<< HEAD:BarcodEZ_Software/XUnitTest/TestClasseAPI.cs
-    public class TestClasseAPI
-=======
     public class XUnitTest1
->>>>>>> ElaborazioneImmagine:BarcodEZ_Software/XUnitTest/XUnitTest1.cs
     {
+        /// <summary>
+        /// Prova a pingare il sito di RapidApi
+        /// </summary>
         [Fact]
-        public void Ping_sito()
+        public void Test1()
         {
             //Arrange
             var ping = new System.Net.NetworkInformation.Ping();
