351dc394c95355e9d539b81bea32a9ceb53b8189
Lucas Testa <info.lucastesta@gmail.com>
2021-04-27 12:58:05 +0200

    Agg

---
+++ BarcodEZ_Software/BarcodEZ_Software/AmazonScraping.cs
@@ -0,0 +1,113 @@
+﻿using System.Net;
+using System.IO;
+using System.Linq;
+using System;
+
+namespace DemoConsoleScraping
+{
+    class AmazonScraping
+    {
+        /// <summary>
+        /// Metodo che estrapola l'HTML della pagina di cui si passa l'URL
+        /// </summary>
+        /// <param name="url">L'URL della pagina web da cui si vuole estrapolare l'HTML</param>
+        /// <returns>La stringa dell'html corrispondente</returns>
+        public static string GetRequest(string url)
+        {
+            string strhtml = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    strhtml = reader.ReadToEnd();
+                }
+                return strhtml;
+            }
+            catch
+            {
+                return strhtml;
+            }
+
+        }
+        /// <summary>
+        /// Metodo che estrae i dati dall'HTML e restituisce un oggetto AmazonProduct contenente vari elementi della pagina Amazon
+        /// </summary>
+        /// <param name="strHtml">L'HTML estrapolato dalla funzione AmazonScraping.GetRequest</param>
+        /// <returns>Oggetto AmazonProduct</returns>
+        public static AmazonProduct DataParse(string strHtml)
+        {
+            string ASIN = string.Empty;
+            string Name = string.Empty;
+            string Price = string.Empty;
+            string FullPrice = string.Empty;
+            string Description = string.Empty;
+            decimal FinalPrice = 0;
+            decimal FinalFullPrice = 0;
+            string[] price = new string[2];
+            string[] fullprice = new string[2];
+
+            //Uso HTML Agility pack
+            HtmlAgilityPack.HtmlDocument htmlDocument = new HtmlAgilityPack.HtmlDocument();
+            htmlDocument.LoadHtml(strHtml);
+            htmlDocument.DocumentNode.Descendants()
+                .Where(n => n.Name == "script" || n.Name == "style")
+                .ToList()
+                .ForEach(n => n.Remove());
+
+            ASIN = htmlDocument.DocumentNode.SelectSingleNode("//table[@id='productDetails_techSpec_section_1']//tr[last()]//td[@class='a-size-base prodDetAttrValue']")?.InnerText.Trim();
+            if (ASIN == null || ASIN.Length != 10)
+            {
+                ASIN = htmlDocument.DocumentNode.SelectSingleNode("//table[@id='productDetails_detailBullets_sections1']//tr[1]//td[@class='a-size-base prodDetAttrValue']")?.InnerText.Trim();
+                if (ASIN == null || ASIN.Length != 10)
+                {
+                    ASIN = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='detailBullets_feature_div']//ul//text()[contains(., 'ASIN')]/ancestor::span[2]//span[2]")?.InnerText.Trim();
+                    if (ASIN == null || ASIN.Length != 10)
+                    {
+                        ASIN = "AMAZONPROD";
+                    }
+                }
+            }
+
+            Name = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='productTitle']").InnerText.Trim();
+
+            Price = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='priceblock_ourprice']")?.InnerText.Trim();
+            if (Price == null)
+            {
+                Price = htmlDocument.DocumentNode.SelectSingleNode("//span[@id='priceblock_dealprice']")?.InnerText.Trim();
+                if (Price == null)
+                {
+                    Price = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='a-color-price']")?.InnerText.Trim();
+                }
+                else
+                {
+                    FullPrice = htmlDocument.DocumentNode.SelectSingleNode("//span[@class='priceBlockStrikePriceString a-text-strike']")?.InnerText.Trim();
+                }
+            }
+
+            Description = htmlDocument.DocumentNode.SelectSingleNode("//div[@id='productDescription']//p")?.InnerText.Trim();
+            if (Description == null)
+            {
+                Description = "Descrizione non disponibile. Visitare la pagina del prodotto";
+            }
+
+            price = Price.Split((char)160);
+            FinalPrice = decimal.Parse(price[0]);
+            if (!(string.Compare(FullPrice, "") == 0))
+            {
+                fullprice = FullPrice.Split((char)160);
+                FinalFullPrice = decimal.Parse(fullprice[0]);
+            }
+            AmazonProduct result = new AmazonProduct(ASIN, Name, FinalPrice, Description, FinalFullPrice);
+
+            return result;
+        }
+    }
+}
+
